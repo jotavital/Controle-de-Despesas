d71ac99d00f02bed5123e9c96c65b6024852a433
jotavital <joaopedrovitaldoctum@gmail.com>
2021-03-16 18:36:39 -0300

    LOGIN v1.0
    Tela de login criada, sem funcionalidade

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/bulma.css">
+    <link rel="stylesheet" href="style/main.css">
+    <title>Controle de Despesas</title>
+</head>
+
+<body>
+    <div id="container">
+        <div id="formLogin">
+            <form action="" method="" id="formulario">
+                <div class="field">
+                    <label class="label">Nome de usuário:</label>
+                    <div class="control">
+                        <input class="input is-rounded" type="text" placeholder="Escolha um nome de usuário">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Senha:</label>
+                    <div class="control">
+                        <input class="input is-rounded" type="password" placeholder="Escolha uma senha forte">
+                    </div>
+                </div>
+                <div class="field is-grouped">
+                    <div class="control" id="enviar" >
+                        <button class="button is-link">Entrar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
