e8f243340be027ebd1839d551a9a5113a95b1dac
jotavital <joaopedrovitaldoctum@gmail.com>
2021-03-19 18:17:15 -0300

    connected to MySQL
    conexão ao banco de dados feita com sucesso e funcional

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    include_once("../connections/conexao.php");
+    $key = uniqid(md5(rand()));
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,6 +19,18 @@
     <div id="container">
         <div class="divForm">
             <form action="#" method="" class="box" id="formulario">
+                <?php 
+                    // para teste de conexão ao banco
+                    // if(isset($_SESSION['msg'])){
+                    //     echo $_SESSION['msg'];
+                    //     unset($_SESSION['msg']);
+                    // }
+                    // if(!$conn){
+                    //     die("<div class='notification is-danger'>" . "Falha na conexão com a base de dados: " . mysqli_connect_error() . ", verifique o arquivo de conexão e tente novamente</div>");
+                    // }else{
+                    //     echo "<div class='notification is-success'>" . "Conexão ao banco de dados realizada com sucesso!" . "<button class='delete' aria-label='Close'></button></div>";
+                    // }
+                ?>
                 <h1 class="title">Entrar</h1>
                 <div class="field">
                     <label class="label" for="usuario">Nome de usuário:</label>
@@ -41,8 +59,10 @@
             </form>
         </div>
     </div>
-    
-<script src="https://kit.fontawesome.com/2ecaa7524c.js" crossorigin="anonymous"></script>
+    <?php 
+        mysqli_close($conn);
+    ?>
+    <script src="https://kit.fontawesome.com/2ecaa7524c.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- pages/register.php
+++ pages/register.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    include_once("../connections/conexao.php");
+    $key = uniqid(md5(rand()));
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -9,15 +15,28 @@
     <link rel="stylesheet" href="/style/main.css">
     <title>Controle de Despesas - Cadastro</title>
 </head>
+
 <body>
     <div id="container">
         <div class="divForm">
             <form action="#" method="" class="box" id="formulario">
+                <?php 
+                    // para teste de conexão ao banco
+                    // if(isset($_SESSION['msg'])){
+                    //     echo $_SESSION['msg'];
+                    //     unset($_SESSION['msg']);
+                    // }
+                    // if(!$conn){
+                    //     die("<div class='notification is-danger'>" . "Falha na conexão com a base de dados: " . mysqli_connect_error() . ", verifique o arquivo de conexão e tente novamente</div>");
+                    // }else{
+                    //     echo "<div class='notification is-success'>" . "Conexão ao banco de dados realizada com sucesso!" . "<button class='delete' aria-label='Close'></button></div>";
+                    // }
+                ?>
                 <h1 class="title">Cadastrar-se</h1>
                 <div class="field">
                     <label class="label" for="nome">Nome:</label>
                     <div class="control">
-                        <input class="input is-rounded" id ="nome" type="text" placeholder="Seu nome">
+                        <input class="input is-rounded" id="nome" type="text" placeholder="Seu nome">
                     </div>
                 </div>
                 <div class="field">
@@ -29,23 +48,26 @@
                 <div class="field">
                     <label class="label" for="usuario">Nome de usuário:</label>
                     <div class="control">
-                        <input class="input is-rounded" id="usuario" type="text" placeholder="Escolha um nome de usuário">
+                        <input class="input is-rounded" id="usuario" type="text"
+                            placeholder="Escolha um nome de usuário">
                     </div>
                 </div>
                 <div class="field">
                     <label class="label" for="senha">Senha:</label>
                     <div class="control">
-                        <input class="input is-rounded" id="senha" type="password" placeholder="Escolha uma senha forte">
+                        <input class="input is-rounded" id="senha" type="password"
+                            placeholder="Escolha uma senha forte">
                     </div>
                 </div>
                 <div class="field">
                     <label class="label" for="confirmaSenha">Confirme a senha:</label>
                     <div class="control">
-                        <input class="input is-rounded" id="confirmaSenha" type="password" placeholder="Digite sua senha novamente">
+                        <input class="input is-rounded" id="confirmaSenha" type="password"
+                            placeholder="Digite sua senha novamente">
                     </div>
                 </div>
                 <div class="block">
-                <p>Já é cadastrado? <a href="login.php">Entre!</a></p>
+                    <p>Já é cadastrado? <a href="login.php">Entre!</a></p>
                 </div>
                 <div class="buttons is-centered">
                     <button class="button is-success">
@@ -58,8 +80,23 @@
             </form>
         </div>
     </div>
-    
-<script src="https://kit.fontawesome.com/2ecaa7524c.js" crossorigin="anonymous"></script>
+    <?php 
+        mysqli_close($conn);
+    ?>
+    <script src="https://kit.fontawesome.com/2ecaa7524c.js" crossorigin="anonymous"></script>
 </body>
+
+<script>
+    // fecha a notificação
+    document.addEventListener('DOMContentLoaded', () => {
+        (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+            const $notification = $delete.parentNode;
+
+            $delete.addEventListener('click', () => {
+                $notification.parentNode.removeChild($notification);
+            });
+        });
+    });
+</script>
 
 </html>
