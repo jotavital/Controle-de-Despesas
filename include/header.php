0f5e2e521bc66a2e923e280892d015a559640205
jotavital <80636554+jotavital@users.noreply.github.com>
2021-04-20 12:49:02 -0300

    Mudei a ordem de importação de css

--- include/header.php
+++ include/header.php
@@ -17,7 +17,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Easylize Finanças - <?php echo $title ?></title>
         <link rel="shortcut icon" href="../image/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="/style/all.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="stylesheet" href="/style/all.css">
     </head>
 <html>
