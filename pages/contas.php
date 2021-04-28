7f77a6ac870be10c2859415355e5b3f7e5cf728b
jotavital <joaopedrovitaldoctum@gmail.com>
2021-04-27 22:47:57 -0300

    Update contas.php
    bug fix no select das categorias da conta

--- pages/contas.php
+++ pages/contas.php
@@ -43,7 +43,7 @@ setTitulo($title);
                                             <select class="form-select" name="categoriaSelect" id="categoriaSelect">
                                                 <?php
                                                     include("connection.php");
-                                                    $sql = $conn->prepare("SELECT * FROM categoria");
+                                                    $sql = $conn->prepare("SELECT * FROM categoria WHERE fk_tipo = 5");
                                                     $sql->execute();
                                                     $data = $sql->fetchAll();
 
