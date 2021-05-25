7257c78bad1a7fcf36c894edcd9d9eecb6438b52
jotavital <joaopedrovitaldoctum@gmail.com>
2021-05-25 00:23:13 -0300

    Update sideNav.php

--- include/sideNav.php
+++ include/sideNav.php
@@ -4,40 +4,53 @@
     </div>
     <div id="sidebarItens">
         <ul>
-            <div class="row">
+            <div class="row mb-1">
                 <li>
                     <i class="fas fa-tachometer-alt"></i>
                     <a href="../pages/dashboard.php">Dashboard</a>
                 </li>
             </div>
-            <li>
-                <i class="fas fa-wallet"></i>
-                <a href="../pages/contas.php">Contas</a>
-            </li>
-            <li>
-                <i class="fas fa-dollar-sign"></i>
-                <a href="../pages/despesas.php">Despesas</a>
-            </li>
-            <li>
-                <i class="fas fa-chart-line"></i>
-                <a href="../pages/receitas.php">Receitas</a>
-            </li>
-            <li>
-                <i class="fas fa-receipt"></i>
-                <a href="">Transações</a>
-            </li>
-            <li>
-                <i class="fas fa-chart-pie"></i>
-                <a href="">Estatísticas</a>
-            </li>
-            <li>
-                <i class="fas fa-bullseye"></i>
-                <a href="">Metas</a>
-            </li>
-            <li>
-                <i class="fas fa-user-circle"></i>
-                <a href="../pages/profile.php">Perfil</a>
-            </li>
+            <div class="row mb-1">
+                <li>
+                    <i class="fas fa-wallet"></i>
+                    <a href="../pages/contas.php">Contas</a>
+                </li>
+            </div>
+            <div class="row mb-1">
+                <li>
+                    <i class="fas fa-dollar-sign"></i>
+                    <a href="../pages/despesas.php">Despesas</a>
+                </li>
+            </div>
+            <div class="row mb-1">
+                <li>
+                    <i class="fas fa-chart-line"></i>
+                    <a href="../pages/receitas.php">Receitas</a>
+                </li>
+            </div>
+            <div class="row mb-1">
+                <li>
+                    <i class="fas fa-receipt"></i>
+                    <a href="">Transações</a>
+                </li>
+            </div>
+            <div class="row mb-1">
+                <li>
+                    <i class="fas fa-chart-pie"></i>
+                    <a href="">Estatísticas</a>
+                </li>
+            </div>
+            <div class="row mb-1">
+                <li>
+                    <i class="fas fa-bullseye"></i>
+                    <a href="">Metas</a>
+                </li>
+            </div>
+            <div class="row mb-1">
+                <li>
+                    <i class="fas fa-user-circle"></i>
+                    <a href="../pages/profile.php">Perfil</a>
+                </li>
         </ul>
         <div class="d-flex justify-content-center">
             <a class="btn btn-outline-danger col-5" href="../connections/logout.php" role="button">Logout</a>
