a1f2a6e7ff897726dbbd20444585e6a1a740b5ad
jotavital <80636554+jotavital@users.noreply.github.com>
2021-05-25 12:28:54 -0300

    Ajustes nos icones da sidebar

--- include/sideNav.php
+++ include/sideNav.php
@@ -2,55 +2,88 @@
     <div id="sidebarHeader">
         <h4 class="col d-flex justify-content-center">Menu</h4>
     </div>
-    <div id="sidebarItens">
+    <div id="sidebarItems">
         <ul>
-            <div class="row mb-1">
-                <li>
-                    <i class="fas fa-tachometer-alt"></i>
-                    <a href="../pages/dashboard.php">Dashboard</a>
-                </li>
-            </div>
-            <div class="row mb-1">
-                <li>
-                    <i class="fas fa-wallet"></i>
-                    <a href="../pages/contas.php">Contas</a>
-                </li>
-            </div>
-            <div class="row mb-1">
-                <li>
-                    <i class="fas fa-dollar-sign"></i>
-                    <a href="../pages/despesas.php">Despesas</a>
-                </li>
-            </div>
-            <div class="row mb-1">
-                <li>
-                    <i class="fas fa-chart-line"></i>
-                    <a href="../pages/receitas.php">Receitas</a>
-                </li>
-            </div>
-            <div class="row mb-1">
-                <li>
-                    <i class="fas fa-receipt"></i>
-                    <a href="">Transações</a>
-                </li>
-            </div>
-            <div class="row mb-1">
-                <li>
-                    <i class="fas fa-chart-pie"></i>
-                    <a href="">Estatísticas</a>
-                </li>
-            </div>
-            <div class="row mb-1">
-                <li>
-                    <i class="fas fa-bullseye"></i>
-                    <a href="">Metas</a>
-                </li>
-            </div>
-            <div class="row mb-1">
-                <li>
-                    <i class="fas fa-user-circle"></i>
-                    <a href="../pages/profile.php">Perfil</a>
-                </li>
+            <li>
+                <div class="mb-1 sideNavItem d-flex align-items-center">
+                    <div class="sideNavIcon">
+                        <i class="fas fa-tachometer-alt"></i>
+                    </div>
+                    <div class="sideNavLink">
+                        <a href="../pages/dashboard.php">Dashboard</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="mb-1 sideNavItem d-flex align-items-center">
+                    <div class="sideNavIcon">
+                        <i class="fas fa-wallet"></i>
+                    </div>
+                    <div class="sideNavLink">
+                        <a href="../pages/contas.php">Contas</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="mb-1 sideNavItem d-flex align-items-center">
+                    <div class="sideNavIcon">
+                        <i class="fas fa-dollar-sign"></i>
+                    </div>
+                    <div class="sideNavLink">
+                        <a href="../pages/despesas.php">Despesas</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="mb-1 sideNavItem d-flex align-items-center">
+                    <div class="sideNavIcon">
+                        <i class="fas fa-chart-line"></i>
+                    </div>
+                    <div class="sideNavLink">
+                        <a href="../pages/receitas.php">Receitas</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="mb-1 sideNavItem d-flex align-items-center">
+                    <div class="sideNavIcon">
+                        <i class="fas fa-receipt"></i>
+                    </div>
+                    <div class="sideNavLink">
+                        <a href="">Transações</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="mb-1 sideNavItem d-flex align-items-center">
+                    <div class="sideNavIcon">
+                        <i class="fas fa-chart-pie"></i>
+                    </div>
+                    <div class="sideNavLink">
+                        <a href="">Estatísticas</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="mb-1 sideNavItem d-flex align-items-center">
+                    <div class="sideNavIcon">
+                        <i class="fas fa-bullseye"></i>
+                    </div>
+                    <div class="sideNavLink">
+                        <a href="">Metas</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="mb-1 sideNavItem d-flex align-items-center">
+                    <div class="sideNavIcon">
+                        <i class="fas fa-user-circle"></i>
+                    </div>
+                    <div class="sideNavLink">
+                        <a href="../pages/profile.php">Perfil</a>
+                    </div>
+                </div>
+            </li>
         </ul>
         <div class="d-flex justify-content-center">
             <a class="btn btn-outline-danger col-5" href="../connections/logout.php" role="button">Logout</a>
