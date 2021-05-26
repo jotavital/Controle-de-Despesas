196bc48664b4f03ebcf65efefc0e2230221cf145
jotavital <joaopedrovitaldoctum@gmail.com>
2021-05-26 16:46:56 -0300

    unifiquei todos os css dentro do all.css

--- include/navBar_logged.php
+++ include/navBar_logged.php
@@ -3,6 +3,6 @@
         <i class="fas fa-bars fa-2x"></i>
     </div>
     <div class="col-11 headerDashboardTitle d-flex justify-content-center">
-        <h4><?php echo $title ?></h4>
+        <h4 class="title"><?php echo $title ?></h4>
     </div>
 </div>
