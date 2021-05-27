0a8f8f2cb7e33a752aef77f14b6e20caee0fed40
jotavital <80636554+jotavital@users.noreply.github.com>
2021-05-27 11:51:35 -0300

    titulo da barra superior da dashboard sempre centralizado

--- include/navBar_logged.php
+++ include/navBar_logged.php
@@ -1,8 +1,8 @@
-<div id="navBarDashboard" class="col-12 navBar">
-    <div class="col-2 menuToggler">
+<div id="navBarDashboard" class="col-12 navBar d-flex justify-content-start">
+    <div class="menuToggler">
         <i class="fas fa-bars fa-2x"></i>
     </div>
-    <div class="col-10 headerDashboardTitle d-flex justify-content-center">
+    <div class="col-12 headerDashboardTitle d-flex justify-content-center">
         <h4 class="title"><?php echo $title ?></h4>
     </div>
 </div>
--- js/sideBarCollapse.js
+++ js/sideBarCollapse.js
@@ -1,21 +1,24 @@
 var menuIconDashboard = document.getElementById("navBarDashboard").querySelector(".menuToggler");
 var menuIconSideBar = document.getElementById("sidebarHeader").querySelector(".menuToggler");
 var sideBar = document.querySelector(".sideBarMenu");
+var title = document.getElementById("navBarDashboard").querySelector(".headerDashboardTitle");
 
 window.onload = function(){
 
-    if(!sideBar.classList.contains("hide")){
+    if(!sideBar.classList.contains("hide")){ //sidebar esta aberta
         menuIconDashboard.classList.add("hide");
     }
 
-    menuIconDashboard.addEventListener("click", function () {
+    menuIconDashboard.addEventListener("click", function () { //abriu a sidebar
         sideBar.classList.toggle("hide");
         menuIconDashboard.classList.add("hide");
+        title.classList.replace("col-11", "col-12");
     });
 
-    menuIconSideBar.addEventListener("click", function(){
+    menuIconSideBar.addEventListener("click", function(){ //fechou a sidebar
         sideBar.classList.toggle("hide");
         menuIconDashboard.classList.remove("hide");
+        title.classList.replace("col-12", "col-11");
     });
 };
 
